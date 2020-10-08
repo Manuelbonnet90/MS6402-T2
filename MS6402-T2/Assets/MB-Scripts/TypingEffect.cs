6a508843691f8111cfb5dc5d5c27f2da3c6884c8
Manuelbonnet90 <58072925+Manuelbonnet90@users.noreply.github.com>
2020-10-08 19:42:17 +0100

    Story StartScreen

---
+++ MS6402-T2/Assets/MB-Scripts/TypingEffect.cs
@@ -0,0 +1,56 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypingEffect : MonoBehaviour
+{
+    public string sentences;
+    public float letterPause = 0.2f;
+
+    public Text storyText;
+
+    public Text countdownText;
+    public float currentTime;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //startTime = Time.time;
+        animator = GetComponent<Animator>();
+        animator.SetBool("IsOpen", true);
+     
+        StartCoroutine(TypeSentence());
+
+        
+    }
+    private void Update()
+    {
+        TimerMenu();
+    }
+    IEnumerator TypeSentence ()
+    {
+        //Debug.Log("Work");
+        foreach(char letter in sentences.ToCharArray())
+        {
+            storyText.text += letter;
+            yield return new WaitForSeconds(letterPause);
+        }
+     
+    }
+
+    void TimerMenu()
+    {
+        countdownText.GetComponent<Text>().enabled = true; // turn on the timer
+        currentTime -= 1 * Time.deltaTime; // create a countdown for the shotgun pick up, each pick is 10 second.
+        countdownText.text = currentTime.ToString("0"); // only display two numbers from the countdown.
+        if (currentTime <= 0)
+        {
+            countdownText.GetComponent<Text>().enabled = false;
+            animator.SetBool("IsOpen", false);
+            StopAllCoroutines();
+        }
+    }
+}
